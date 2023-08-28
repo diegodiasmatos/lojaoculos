0e8be5a87ebef3b055eabf9327f6de4bac68af3d
Diego <diegoevelyndiasoliveira@gmail.com>
2023-08-28 01:01:00 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Topo from './componentes/topo';
+import Conteudo from './componentes/Conteudo';
+import Footer from './componentes/rodape';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    <Topo />
+    <Conteudo />
+    <Footer />
+
+    </div>
+  );
+}
+
+export default App;
